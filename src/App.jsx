5258ab32ac9528e40183f4f53a3947d1708bef01
shubham8456 <shubham8456@gmail.com>
2024-01-26 17:15:05 +0530

    optimize use of multiple similar components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import reactImg from './assets/react-core-concepts.png';
-import componentsImg from './assets/components.png';
+import { CORE_CONCEPTS } from './data.js';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -22,12 +22,12 @@ function Header() {
   );
 }
 
-function CoreConcept(props) {
+function CoreConcept({ image, title, description }) {
   return (
     <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
     </li>
   );
 }
@@ -40,26 +40,10 @@ function App() {
         <section id="core-concepts">
           <h2>Time to get started!</h2>
           <ul>
-            <CoreConcept
-              title = "Components"
-              description = "The core UI building block."
-              image = {componentsImg}
-            />
-            <CoreConcept
-              title = "Props"
-              description = "The core UI building block."
-              image = {componentsImg}
-            />
-            <CoreConcept
-              title = "Shubham"
-              description = "The core UI building block."
-              image = {componentsImg}
-            />
-            <CoreConcept
-              title = "Rawat"
-              description = "The core UI building block."
-              image = {componentsImg}
-            />
+            <CoreConcept {...CORE_CONCEPTS[0]}/>
+            <CoreConcept {...CORE_CONCEPTS[1]}/>
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/>
           </ul>
         </section>
       </main>
