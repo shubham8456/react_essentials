cd232747a21b217502e79cccb035725df9f3152a
Shubham Rawat <shubham8456@gmail.com>
2024-04-05 14:21:06 +0530

    Learn React basics (#1)
    * intial_changes
    
    * creating dynamic components
    
    * optimize use of multiple similar components
    
    * moved components into a separate directory
    
    * moving components into separate folders with css
    
    * created button with props.children
    
    * onClick event on Button
    
    * created onClick on custom Component
    
    * sending custom values in onClick using arrow function, to determine which button is pressed
    
    * adding just normal variable doesnt execute the component again
    
    * useState to execute component function again when a state is changed
    
    * loading examples data from seed_data file
    
    * display default value or  custom value when a button is clicked

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,55 @@
+import { useState } from 'react';
+
+import { CORE_CONCEPTS, EXAMPLES } from './data.js';
+import Header from './components/Header/Header.jsx';
+import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
+
 function App() {
+  // hooks like useState should be decalred right at top level of the component
+  // functions, and not in inner functions, conditionals or loop
+
+  const [selectedTopic, setSelectedTopic] = useState(null);
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (<div id="tab-content">
+      <h3>{EXAMPLES[selectedTopic].title}</h3>
+      <p>{EXAMPLES[selectedTopic].description}</p>
+      <pre>
+        <code>{EXAMPLES[selectedTopic].code}</code>
+      </pre>
+    </div>);
+  }
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Time to get started!</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+          {tabContent}
+        </section>
       </main>
     </div>
   );
