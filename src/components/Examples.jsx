fd646d190acb880d759ada2f6a80eebb02bb3e91
Shubham Rawat <shubham.rawat@plansource.com>
2024-08-30 19:28:51 +0530

    Lecture 67 | created another Tabs component which will have a menu html element and content

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import TabButton from "./TabButton";
 import Section from "./Section";
+import Tabs from "./Tabs";
 import { EXAMPLES } from "../data";
 
 export default function Examples() {
@@ -28,13 +29,18 @@ export default function Examples() {
 
   return (
     <Section title="Examples" id="examples">
-      <menu>
-        <TabButton isSelected={selectedTopic === 'components'} onClick={() => handleSelect('components')}>Components</TabButton>
-        <TabButton isSelected={selectedTopic === 'jsx'}        onClick={() => handleSelect('jsx')       }>JSX</TabButton>
-        <TabButton isSelected={selectedTopic === 'props'}      onClick={() => handleSelect('props')     }>Props</TabButton>
-        <TabButton isSelected={selectedTopic === 'state'}      onClick={() => handleSelect('state')     }>State</TabButton>
-      </menu>
-      {tabContent}
+      <Tabs
+        buttons={
+          <>
+            <TabButton isSelected={selectedTopic === 'components'} onClick={() => handleSelect('components')}>Components</TabButton>
+            <TabButton isSelected={selectedTopic === 'jsx'}        onClick={() => handleSelect('jsx')       }>JSX</TabButton>
+            <TabButton isSelected={selectedTopic === 'props'}      onClick={() => handleSelect('props')     }>Props</TabButton>
+            <TabButton isSelected={selectedTopic === 'state'}      onClick={() => handleSelect('state')     }>State</TabButton>
+          </>
+        }
+      >
+        {tabContent}
+      </Tabs>
     </Section>
   );
 }
